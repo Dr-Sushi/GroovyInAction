7eaeb06bd584cf07ec992a5f080c2804332c6691
Dr-sushi <sju.akng@gmail.com>
2015-07-29 23:33:34 -0500

    Initial commit

---
+++ GroovyInActionBook/src/main/java/Book.groovy
@@ -0,0 +1,12 @@
+/**
+ * Created by root on 7/29/15.
+ */
+class Book {
+    private String title
+    Book (String theTitle) {
+        title = theTitle
+    }
+    String getTitle(){
+        return title
+    }
+}
