e672b70ee4bbfe8880cba9221ee0e5f17ffc901a
Dr-sushi <sju.akng@gmail.com>
2015-07-30 23:50:59 -0500

    More chapter 2 code

--- GroovyInActionBook/src/main/resources/Script/BookScript.groovy
+++ GroovyInActionBook/src/main/resources/Script/BookScript.groovy
@@ -14,3 +14,16 @@ String getTitleBackwards(book) {
     String title = book.getTitle()
     return title.reverse()
 }
+
+class BookBean {
+    String title
+}
+
+def groovyBook = new BookBean()
+
+groovyBook.setTitle('Groovy in Action')
+assert groovyBook.getTitle() == 'Groovy in Action'
+
+//Utilizes accessors and is not field access
+groovyBook.title = 'Groovy Conquers the world'
+assert groovyBook.title == 'Groovy Conquers the world'
